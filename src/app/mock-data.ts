237e83e0f1c3627e6d0f90544ca06c45e742fd7f
Vachet Virginie <vachetvirginie@gmail.com>
2017-10-19 11:36:23 +0200

    test

---
+++ src/app/mock-data.ts
@@ -0,0 +1,177 @@
+export const DATA: any  = {
+  "categories": [
+    {
+      "categori_id": 1,
+      "name": "Offres spéciales "
+    },
+    {
+      "categori_id": 2,
+      "name": "Action"
+    },
+    {
+      "categori_id": 3,
+      "name": "Aventure"
+    },
+    {
+      "categori_id": 4,
+      "name": "Course automobile"
+    }
+    {
+      "categori_id": 5,
+      "name": "Independant"
+    }
+    {
+      "categori_id": 6,
+      "name": "RPG"
+    }
+    {
+      "categori_id": 7,
+      "name": "Simulation"
+    }
+    {
+      "categori_id": 8,
+      "name": "Sport"
+    }
+    {
+      "categori_id": 9,
+      "name": "Strategie"
+    }
+  ],
+  "products": [
+    {
+      "id": 1,
+      "name": "The Witcher II",
+      "price": "20",
+      "available": true,
+      "best_seller": true,
+      "categories": [
+        1,
+       
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/379770/capsule_616x353_french.jpg?t=1508176888",
+      "description": "The Witcher Franchise. Up to 85% off!"
+    },
+    {
+      "id": 2,
+      "name": "Freaky Awesome",
+      "price": "50",
+      "available": true,
+      "best_seller": false,
+      "categories": [
+        2
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/622370/header.jpg?t=1508338170",
+      "description": "Freaky Awesome est un rogue-lite à l'action trépidante. Vous explorez une vieille usine générée aléatoirement, combattez de curieux ennemis et évitez divers pièges. Les déchets toxiques vous font muter, vous octroyant ainsi des pouvoirs différents à chaque fois."
+    },
+    {
+      "id": 3,
+      "name": "The Pirate: Plague of the Dead",
+      "price": "40",
+      "available": true,
+      "best_seller": true,
+      "categories": [
+        3
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/723790/header.jpg?t=1507915293",
+      "description": "Commandez les pirates les plus terrifiants sur la Mer des Caraïbes! Plague of the Dead, c’est un nouvel épisode de la série The Pirate. En présentant le cycle du jour et de la nuit, et un système de météo, le jeu vidéo offre une expérience inoubliable pour chaque joueur. "
+    },
+    {
+      "id": 4,
+      "name": "Road Redemption",
+      "price": "35",
+      "available": true,
+      "best_seller": false,
+      "categories": [
+       4
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/300380/header.jpg?t=1507690643",
+      "description": "Road Redemption est un jeu de course d'action où vous dirigez votre gang de motard lors d'un voyage époque à travers le pays dans cette aventure où vous conduirez et ferez mordre le bitume à vos adversaire. "
+    },
+    {
+      "id": 5,
+      "name": "Luna",
+      "price": "42",
+      "available": true,
+      "best_seller": true,
+      "categories": [
+        5
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/605770/header.jpg?t=1508256034",
+      "description": "Luna est une fable VR interactive sur PC. Résolvez des casse-têtes célestes et faites pousser des jardins féériques dans vos terrariums musicaux. Réveillez de sombres forêts, redonnez vie à des lacs chatoyants et découvrez des créatures mystérieuses."
+    },
+    {
+      "id": 6,
+      "name": "Warbanners",
+      "price": "50",
+      "available": true,
+      "best_seller": false,
+      "categories": [
+       6
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/650440/header.jpg?t=1508335240",
+      "description": "Warbanners est un jeu au tour par tour mélangeant stratégie tactique avec des éléments de jeu de rôle. Tout en gérant une escouade de mercenaires, le joueur pourra faire l'expérience d'une histoire s'étalant sur une campagne de 42 missions"
+    },
+    {
+      "id": 7,
+      "name": "Gold Rush: The Game",
+      "price": "55",
+      "available": true,
+      "best_seller": false,
+      "categories": [
+        7
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/451340/header.jpg?t=1508251067",
+      "description": "Become the gold miner. Work hard, dig deep, explore the world, and you’ll become the wealthiest man in Alaska. Use a variety of highly-detailed machines to find as much gold as you can."
+    },
+    {
+      "id": 8,
+      "name": "Rocket League®",
+      "price": "55",
+      "available": true,
+      "best_seller": false,
+      "categories": [
+        8
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/252950/header.jpg?t=1507673535",
+      "description": "Soccer meets driving once again in the long-awaited, physics-based multiplayer-focused sequel to Supersonic Acrobatic Rocket-Powered Battle-Cars! Choose a variety of high-flying vehicles equipped with huge rocket boosters to score amazing aerial goals and pull-off incredible game-changing saves!"
+    },
+    {
+      "id": 9,
+      "name": "Dungeons 3",
+      "price": "65",
+      "available": true,
+      "best_seller": true,
+      "categories": [
+        9
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/493900/header.jpg?t=1508260911/",
+      "description": "Le seigneur du donjon a enfin réussi à unifier les forces du mal sous un seul et même terrible empire. Prochaine étape de sa quête diabolique : l'expansion ! "
+    },
+    {
+      "id": 10,
+      "name": "The Escapists 2",
+      "price": "65",
+      "available": true,
+      "best_seller": true,
+      "categories": [
+        9,
+        5
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/641990/header.jpg?t=1505727903",
+      "description": "Fabriquez, volez, combattez et évadez-vous ! C'est l'heure de s'échapper des pires prisons du monde alors que vous retrouvez votre vie carcérale dans The Escapists 2, désormais avec le mode multijoueur ! Parviendrez-vous à vous évader ?  "
+    },
+    {
+      "id": 11,
+      "name": "H1Z1: King of the Kill",
+      "price": "20",
+      "available": false,
+      "best_seller": true,
+      "categories": [
+        1
+      ],
+      "img": "http://cdn.edgecast.steamstatic.com/steam/apps/433850/header.jpg?t=1500574556",
+      "description": "H1Z1: King of the Kill is a fast-paced shooter in which players compete in large-scale chaotic PvP spectacles of skill, wit and a little luck, where everyone must fight to the death to stand alone at the top of the podium. "
+    }
+  ]
+  
+}
